64bb483d9361525483b6d838f522233c57d20626
Paolo86 <p.ferri1986@gmail.com>
2019-10-19 12:48:30 +0800

    Comments and renaming

--- signalHandler.c
+++ signalHandler.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <signal.h>
 
-void CatchSignal(int signalNumber)
+void SignalHandler(int signalNumber)
 {
    // Set validCommand to false so the line written won't make it into the toknizer in main.c
    *setValidCommand = 0;   
@@ -15,16 +15,15 @@ void SetUpSignal(int* validCommand)
 
         struct sigaction act;
 	act.sa_flags = 0;
-	act.sa_handler = CatchSignal;
+	act.sa_handler = SignalHandler;
 
 	sigaction(SIGINT, &act, NULL);
 	sigaction(SIGQUIT, &act, NULL);
 	sigaction(SIGKILL, &act, NULL);
 
 	// Set up handler for SIGCHLD signal (see executoner.c)
-  	struct sigaction childDone;
-		
-  	//childDone.sa_flags = SA_RESTART;
+  	struct sigaction childDone;		
+
   	childDone.sa_flags = SA_SIGINFO;
   	childDone.sa_sigaction = ChildHandler;
   	sigaction(SIGCHLD,&childDone,NULL);
--- signalHandler.h
+++ signalHandler.h
@@ -1,7 +1,10 @@
 #ifndef SIGNALHANDLER_H
 #define SIGNALHANDLER_H
 
-void CatchSignal(int signalNumber);
+// Set "validCommand" to 0 so the input is not sent to the tokenizer
+void SignalHandler(int signalNumber);
+
+// Set up signalhandlers
 void SetUpSignal(int* validCommand);
 
 int* setValidCommand;
