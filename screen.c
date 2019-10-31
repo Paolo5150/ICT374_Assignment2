442337ebe05638adb3795698ef6f4955b47bd102
Paolo86 <p.ferri1986@gmail.com>
2019-10-31 17:44:04 +0800

    Fixed prompt

--- screen.c
+++ screen.c
@@ -17,11 +17,11 @@ void SplashScreen()
 void InitializeScreen(Screen* screenStruct)
 {
   (*screenStruct).shellPrompt[0] = '%';
-  (*screenStruct).shellPrompt[1] = '>';
+  (*screenStruct).shellPrompt[1] = ' ';
 }
 
 void ChangeShellPrompt(Screen* screenStruct,char* newPrompt)
 {  
   strcpy(screenStruct->shellPrompt,newPrompt);
-  strcat(screenStruct->shellPrompt,">");
+  strcat(screenStruct->shellPrompt," ");
 }
